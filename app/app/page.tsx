d05c982c0d177b8f3303798fdcecc25adcdbdc85
shamikfresh <shamik@freshtri.com>
2025-11-09 09:17:27 -0800

    remove deprecation

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
           WARNING: This demo is not secure. Do not upload private information.
         </p>
         <br/>
-        <h1 style={{fontStyle: "italic"}}>THIS WEBSITE IS INDEFINITELY OFFLINE DUE TO INFRASTRUCTURE DEPRECATIONS</h1>
         <FilesForm serverFormHandler={serverFormHandler}/>
       </div>
     </div>
